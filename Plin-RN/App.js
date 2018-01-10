8d14c99162b6a85dd255d1e7e47fd953dbc74a2a
ericfalchier <ericfalchier@gmail.com>
2018-01-09 20:19:45 -0500

    add react-native-vector-icons

--- Plin-RN/App.js
+++ Plin-RN/App.js
@@ -6,10 +6,11 @@ import { StackNavigator } from 'react-navigation';
 
 
 const PlinApp = StackNavigator({
+    /*
     Home: { screen: HomeScreen,
         navigationOptions: {title: 'Event Map', header: null }},
     Profile: { screen: ProfileScreen,
-        navigationOptions: {title: 'Profile', header: null }},
+        navigationOptions: {title: 'Profile', header: null }},*/
     NewEvent: { screen: NewEventScreen,
         navigationOptions: {title: 'Add Event', header: null }},
 });
@@ -18,7 +19,7 @@ const PlinApp = StackNavigator({
 export default class App extends React.Component {
   render() {
     return (
-        <PlinApp/>
+        <PlinApp />
     );
   }
 }
