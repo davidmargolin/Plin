b5f87fbd307ff55ee4e112253c841f0f58e77511
ericfalchier <ericfalchier@gmail.com>
2018-01-09 20:47:26 -0500

    add icons, date button for possible calandar

--- Plin-RN/Pages/NewEventScreen.js
+++ Plin-RN/Pages/NewEventScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {Text, View, Button, TextInput, StyleSheet } from "react-native";
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 export default class NewEventScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            date: '',
             event_name: '',
             description: '',
             location: '',
@@ -28,28 +31,41 @@ export default class NewEventScreen extends React.Component {
         this.props.navigation.goBack();
     }
 
+    showCalender() {
+        //Date Button Going to look for calandar we can show here onPress. 
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.header}>Create a new event here</Text>
+                <Button 
+                    style={styles.date}
+                    title={this.state.date ? this.state.date : 'Date'}
+                    onPress={()=>this.showCalender()}
+                />
                 <TextInput
                     style={styles.eventInput}
                     onChangeText={(event_name) => this.setState({event_name})}
-                    placeholder="Name"
+                    placeholder="Event Name"
                     value={this.state.event_name}
                 />
+                <MaterialIcon name="event" size={30} color="#900" />
                 <TextInput
                     style={styles.eventInput}
                     onChangeText={(description) => this.setState({description})}
                     placeholder="Description"
                     value={this.state.description}
                 />
+                <MaterialIcon name="description" size={30} color="#900" />
                 <TextInput
                     style={styles.eventInput}
                     onChangeText={(location) => this.setState({location})}
                     placeholder="Location"
                     value={this.state.location}
                 />
+                <EntypoIcon name="location" size={30} color="#900" />
+                <MaterialIcon name="date-range" size={30} color="#900" />
                 <Button
                     style={styles.submitEvent}
                     title="New Event"
@@ -70,8 +86,8 @@ const styles = StyleSheet.create({
     },
     eventInput: {
         fontSize: 20,
-        height: 100,
-        margin: 50,
+        height: 80,
+        margin: 20,
     }
 
 });
